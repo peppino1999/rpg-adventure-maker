012e5a66a63edc2f9d217395588786772e56d4da
pierdomenico.reitano <pierdomenico.reitano@farport.co>
2024-07-04 22:34:54 +0200

    chore: Update mock server command to use nodemon

---
+++ json-server.js
@@ -0,0 +1,16 @@
+const jsonServer = require('json-server');
+const auth = require('json-server-auth');
+const generatePartyId = require('./json-server-middlewares/partyId-generation');
+const app = jsonServer.create();
+const router = jsonServer.router('db.json');
+const bodyParser = require('body-parser');
+
+app.db = router.db
+app.use(bodyParser.json());
+app.use(generatePartyId)
+app.use(auth)
+app.use(router)
+app.listen(3000, () => {
+  console.log('JSON Server is running')
+})
+
