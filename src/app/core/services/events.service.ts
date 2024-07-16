30d547d1bbef3892fe52aa1ee2a4745544849950
Giuseppe Cito <giuseppe.cito48@gmail.com>
2024-07-16 11:21:01 +0200

    feat: Add event list to events component

--- src/app/core/services/events.service.ts
+++ src/app/core/services/events.service.ts
@@ -1,21 +1,27 @@
 import { inject, Injectable } from '@angular/core';
 import { EssentialService } from './essentialService';
-import { Observable, tap } from 'rxjs';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { AuthService } from './auth.service';
 import { GameEvent } from '../models/events';
+import { HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventsService extends EssentialService{
   authService = inject(AuthService)
+  private eventsSubject = new BehaviorSubject<GameEvent[]>([])
+
+  get events$(): Observable<GameEvent[]> {
+    return this.eventsSubject.asObservable();
+  }
 
   constructor() {
     super();
     this.apiPath = 'events'
   }
 
-  createEvent(body: Partial<GameEvent>): Observable<GameEvent>{
+  createEvent(body: Partial<GameEvent>): void{
   const partyId = this.authService.partyId
   const today = new Date()
 
@@ -25,7 +31,7 @@ export class EventsService extends EssentialService{
     creationDate: today.getTime()
   }
 
-  return this.apiCall<GameEvent>(
+  this.apiCall<GameEvent>(
     {
       type: 'POST',
       url: this.apiUrl,
@@ -33,16 +39,33 @@ export class EventsService extends EssentialService{
         body: data
       }
     }
-  ).pipe(
-    tap(
-      _res =>{
+  ).subscribe((res)=>{
+    if(res){
+      this.getEvents()
         this.snackbar.open('Evento Creato con successo', 'chiudi',
           {
             duration: 2000
           }
         )
+    }
       }
     )
-  )
+  }
+
+  getEvents(): void{
+     this.apiCall<GameEvent[]>({
+      type: 'GET',
+      url: this.apiUrl,
+      options:{
+       params: new HttpParams()
+       .set('partyId', this.authService.currentUser.partyId)
+       .set('_sort', 'creationDate')
+       .set('_order', 'desc')
+      }
+    }).subscribe({
+      next: (res) =>{
+        this.eventsSubject.next(res)
+      }
+    })
   }
 }
--- src/app/features/home/events/events.component.ts
+++ src/app/features/home/events/events.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
 import { UserTypes } from '../../../core/models/users';
 import { AuthService } from '../../../core/services/auth.service';
 import { EventForm } from '../../../core/configs/events.config';
@@ -6,20 +6,27 @@ import { EventsService } from '../../../core/services/events.service';
 import { EssentialComponent } from '../../../core/components/essentialComponent';
 import { GameEvent } from '../../../core/models/events';
 
+
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
   styleUrl: './events.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class EventsComponent extends EssentialComponent {
+export class EventsComponent extends EssentialComponent implements OnInit{
+ 
   authService = inject(AuthService);
   eventsService = inject(EventsService);
   userTypes = UserTypes;
   currentUserTypes = this.authService.currentUser.type;
   formConfig = EventForm;
+  events$ = this.eventsService.events$;
+
+ ngOnInit() {
+   this.eventsService.getEvents();
+  }
 
   handleCreation(event: Partial<GameEvent>) {
-    this.takeUntilDestroy(this.eventsService.createEvent(event)).subscribe();
+    this.eventsService.createEvent(event)
   }
 }
